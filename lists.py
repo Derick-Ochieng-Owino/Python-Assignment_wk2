c9b7c055d444c3a360fb66bca08f5640dec33131
Decko <derickrevived777@gmail.com>
2025-03-29 10:01:33 +0300

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,17 @@
+my_list = []
+
+my_list.append(10)
+mylist.append(20)
+mylist.append(30)
+mylist.append(40)
+
+my_list.insert(1,15)
+
+mylist.extend(50,60,70)
+
+my_list.pop()
+
+mylist.sort()
+
+value_index30 = mylist.index(30)
+print(f"The value of index 30 is {value_index30}")
